d3269098ca94b5f55534484ace99ab5296cf3b41
lc-dprakash <divya.prakash@lucrisma.com>
2022-11-08 08:02:29 +0530

    action added

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ var table; // use a global for the submit and return data rendering in the examp
 $(document).ready(function () {
     table = $('#example').DataTable({
         "ajax": "../data.json",
-        columnDefs: [
+        columns: [
+            { data: null, render: function ( data, type, row ) {
+                // Combine the first and last names into a single table field
+                return data.first_name+' '+data.last_name;
+            } },
+            { data: "position" },
+            { data: "office" },
+            { data: "extn" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) },
             {
-                targets: -1,
-                data: null,
-                defaultContent: '<button>Click!</button>',
+                data: null, 
+                className: "dt-center editor-edit",
+                defaultContent: '<button><i class="fa fa-pencil"/></button>',
+                orderable: false
             },
-            
-        ],
+            {
+                data: null, 
+                className: "dt-center editor-delete",
+                defaultContent: '<button><i class="fa fa-trash"/></button>',
+                orderable: false
+            }
+        ]
     });
 
 
     $('#example tbody').on('click', 'button', function () {
+        debugger;
         var data = table.row($(this).parents('tr')).data();
-        alert(data[0] + "'s salary is: " + data[5]);
+        alert(data.first_name + "'s salary is: " + data.salary);
     });
 } );
