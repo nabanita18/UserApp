79d2e0dffbb1620c43eea59a28f0f992f1274882
lc-dprakash <divya.prakash@lucrisma.com>
2022-11-08 07:34:08 +0530

    table added

--- app.js
+++ app.js
@@ -1,62 +1,21 @@
-var editor; // use a global for the submit and return data rendering in the examples
+var table; // use a global for the submit and return data rendering in the examples
  
 $(document).ready(function () {
-    $('#example').DataTable({
+    table = $('#example').DataTable({
         "ajax": "../data.json",
-        "fields": [ {
-                "label": "First name:",
-                "name": "first_name"
-            }, {
-                "label": "Last name:",
-                "name": "last_name"
-            }, {
-                "label": "Position:",
-                "name": "position"
-            }, {
-                "label": "Office:",
-                "name": "office"
-            }, {
-                "label": "Extension:",
-                "name": "extn"
-            }, {
-                "label": "Start date:",
-                "name": "start_date",
-                "type": "datetime"
-            }, {
-                "label": "Salary:",
-                "name": "salary"
-            }
-        ]
-    } );
- 
-    // New record
-    $('a.editor-create').on('click', function (e) {
-        e.preventDefault();
- 
-        editor.create( {
-            title: 'Create new record',
-            buttons: 'Add'
-        } );
-    } );
- 
-    // Edit record
-    $('#example').on('click', 'td.editor-edit', function (e) {
-        e.preventDefault();
- 
-        editor.edit( $(this).closest('tr'), {
-            title: 'Edit record',
-            buttons: 'Update'
-        } );
-    } );
- 
-    // Delete a record
-    $('#example').on('click', 'td.editor-delete', function (e) {
-        e.preventDefault();
- 
-        editor.remove( $(this).closest('tr'), {
-            title: 'Delete record',
-            message: 'Are you sure you wish to remove this record?',
-            buttons: 'Delete'
-        } );
-    } );
+        columnDefs: [
+            {
+                targets: -1,
+                data: null,
+                defaultContent: '<button>Click!</button>',
+            },
+            
+        ],
+    });
+
+
+    $('#example tbody').on('click', 'button', function () {
+        var data = table.row($(this).parents('tr')).data();
+        alert(data[0] + "'s salary is: " + data[5]);
+    });
 } );
